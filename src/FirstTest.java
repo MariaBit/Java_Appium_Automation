488068ed7c83edf46527a7940d48d53192732cd5
MariaBit <mbit1480@gmail.com>
2022-08-06 20:36:47 +0300

    Merge branch 'master' of https://github.com/MariaBit/Java_Appium_Automation

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -33,6 +33,7 @@ public class FirstTest {
         capabilities.setCapability("app","C:\\Users\\98855\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
 
